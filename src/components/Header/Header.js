eec407de3323ba597c45d480096dcf59ad573194
Andrey Makhmutov <dron369@ya.ru>
2021-06-25 02:38:31 +0500

    feat: Add adaptive @media

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -7,11 +7,10 @@ function Header(props) {
   const isAuth = props.auth;
   if (isAuth) {
     return (
-      <header className="header">
+      <header className="header header_auth">
         <Link to="/" className="header__logo"></Link>
         <nav className="header__links">
           <Navigation />
-
         </nav>
         <button className={`header__button ${props.isSideMenuOpen ? 'header__button_close' : ''}`} type="button" aria-label="Открыть меню" onClick={props.onOpenSideMenu}></button>
       </header>
@@ -19,13 +18,12 @@ function Header(props) {
   }
   else {
     return (
-      <header className="header"  style={{backgroundColor: "#073042"}}>
+      <header className="header header"  style={{backgroundColor: "#073042"}}>
         <Link to="/" className="header__logo"></Link>
         <nav className="header__links">
           <Link to="/signup" className="header__link">Регистрация</Link>
           <Link to="/signin" className="header__link header__link_signin">Войти</Link>
         </nav>
-        <button className={`header__button ${props.isSideMenuOpen ? 'header__button_close' : ''}`} type="button" aria-label="Открыть меню" onClick={props.onOpenSideMenu}></button>
       </header>
     );
   }
--- src/components/Main/Promo/Promo.js
+++ src/components/Main/Promo/Promo.js
@@ -3,12 +3,16 @@ import React from 'react';
 function Promo() {
   return (
     <section className="promo">
+      <div className="promo__logo"></div>
       <div className="promo__description">
-        <h1 className="promo__title">Учебный проект студента факультета<br/>Веб-разработки.</h1>
-        <p className="promo__subtitle">Листайте ниже, чтобы узнать больше про этот<br/> проект и его создателя.</p>
+        <h1 className="promo__title">
+          Учебный проект студента факультета Веб-разработки.
+        </h1>
+        <p className="promo__subtitle">Листайте ниже, чтобы узнать больше про этот
+          <span> проект и его создателя.</span></p>
         <a href="#about-project" className="promo__link">Узнать больше</a>
       </div>
-      <div className="promo__logo"></div>
+
     </section>
   );
 }
